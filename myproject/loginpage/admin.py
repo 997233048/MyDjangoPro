da02c6c93f95b4260e4661762532547f827a11fd
997233048 <ali.aamir3048@gmail.com>
2021-03-28 10:06:45 +0530

    updated urls for loginpage

--- myproject/loginpage/admin.py
+++ myproject/loginpage/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from loginpage.models import UserProfileInfo, User
 
 # Register your models here.
+admin.site.register(UserProfileInfo)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url,include
+from loginpage import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^special/', views.special, name='special'),
+    url(r'^dappx/', include('loginpage.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
